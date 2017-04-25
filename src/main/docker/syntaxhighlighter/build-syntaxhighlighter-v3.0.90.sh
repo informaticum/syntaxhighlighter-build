8587068a920d8508ad9688924622f811f7bb9f7d
Stefan Gasterstädt <gasterstaedt@ehealthexperts.de>
2017-04-25 21:33:33 +0200

    No need to install "bower" and "grunt" preparativly

--- src/main/docker/syntaxhighlighter/build-syntaxhighlighter-v3.0.90.sh
+++ src/main/docker/syntaxhighlighter/build-syntaxhighlighter-v3.0.90.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 
-# In order to build "SyntaxHighlighter v3.0.90" [1], you might want to read the
-# bit more verbose guide by Blaise Liu [2]. Beside, you need an old "Node.js".
+# In order to build "SyntaxHighlighter v3.0.90" [1], you need an old "Node.js".
 # Otherwise, when running "grunt" an error is thrown:
 #     Error: Module did not self-register
 #
-# Instead of fixing this situation by installing the old "Node.js" [3], I prefer
-# the containered way [4]:
+# Instead of fixing this situation by installing the old "Node.js" [2], I prefer
+# the containered way [3]:
 #     docker run -it --rm --name node-build-env -v "$PWD":/home/bob/workspace \
 #            node:0.10.32 \
 #            /bin/bash
@@ -16,11 +15,10 @@
 # April 23rd, 2017
 #
 # ---
-# [1] https://github.com/syntaxhighlighter/syntaxhighlighter/tree/v3.0.90
-# [2] http://www.blaiseliu.com/build-the-latest-version-of-syntaxhighlighter/
-# [3] http://www.saintsatplay.com/blog/2015/03/resolving-node-module-did-not-self-register-errors
-# [4] https://hub.docker.com/_/node/
+# [1] https://github.com/syntaxhighlighter/syntaxhighlighter/tree/v3.0.90#building
+# [2] http://www.saintsatplay.com/blog/2015/03/resolving-node-module-did-not-self-register-errors
+# [3] https://hub.docker.com/_/node/
 
-bower install
 npm install
-grunt build
+./node_modules/.bin/bower install
+./node_modules/.bin/grunt build
